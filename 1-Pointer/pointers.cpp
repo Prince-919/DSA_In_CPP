da04680302482cf461d6902bf7842548a5bee0be
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-07 09:35:51 -0800

    first Commit

---
+++ 1-Pointer/pointers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=10;
+    cout<<&i<<endl;
+    int *p = &i;
+    cout<<p<<endl;
+    cout<<*p<<endl;
+
+  //  float f = 10.2;
+  //  float *pf = &f;
+
+   // double d = 122.32;
+   // double *pf = &d;
+    return 0;
+}
---
+++ 1-Pointer/pointers3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<i<<endl;
+    i++;
+    cout<<i<<endl;
+
+    int *p=0;
+    cout<<p<<endl;
+    cout<<*p<<endl;
+    (*p)++;
+    cout<<*p<<endl;
+    return 0;
+}
