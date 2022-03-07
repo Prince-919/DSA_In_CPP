c394259e34b4958dafd612e981eb8f0186fe7a3b
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-07 11:21:54 -0800

    changing function array

---
+++ 1-Pointer/functions_array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int print(int *a, int size)
+{
+    int ans=0;
+    for(int i=0; i<size; i++)
+    {
+        ans +=a[i];
+    }
+    return ans;
+}
+int main()
+{
+    int a[10];
+    cout<<sizeof(a)<<endl;
+    cout<<print(a+3, 7)<<endl;
+    
+    return 0;
+}
