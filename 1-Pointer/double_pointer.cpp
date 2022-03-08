2d1bbc855d56890502d071f3db3628dba65a3dfa
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-07 18:49:51 -0800

    changing double pointer

---
+++ 1-Pointer/double_pointer.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void increment(int **p)
+{
+    p=p+1;
+}
+void increment2(int **p)
+{
+    *p=*p+1;
+}
+void increment3(int **p)
+{
+    **p=**p+1;
+}
+int  main()
+{
+    int i=10;
+    int *p = &i;
+    int **p2 =&p;
+    cout<<p2<<endl;
+    cout<<&p<<endl; 
+
+    cout<<p<<endl;
+    cout<<*p2<<endl;
+    cout<<&i<<endl;
+
+    cout<<i<<endl;
+    cout<<*p<<endl;
+    cout<<**p2<<endl;
+
+    
+
+    return 0;
+}
