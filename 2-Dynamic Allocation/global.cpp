6a97a78cddf271ac2769bbedbb2032879ecea338
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-09 05:55:32 -0800

    changing global

---
+++ 2-Dynamic Allocation/global.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int a;
+
+void g()
+{
+    a++;
+    cout<<a<<endl;
+}
+void f()
+{
+    cout<<a<<endl;
+    a++;
+    g();
+
+}
+ int main()
+ {
+     a =10;
+     f();
+     f();
+     cout<<a<<endl;
+     return 0;
+ }
