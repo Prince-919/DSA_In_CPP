8272d0119c434b85df8f678fa775d8de606cdb44
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-11 18:14:49 -0800

    create fibanacci number

---
+++ Recursion/Fibanacci_Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int fiba(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    int smallOutput1 = fiba(n - 1);
+    int smallOutput2 = fiba(n - 2);
+    return smallOutput1 + smallOutput2;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fiba(n) << endl;
+    return 0;
+}
